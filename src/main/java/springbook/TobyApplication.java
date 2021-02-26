a3725cf5c8ec432dbdc460f7c960f636c18afc28
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-27 01:14:14 +0900

    Change package

--- src/main/java/springbook/TobyApplication.java
+++ src/main/java/springbook/TobyApplication.java
@@ -1,4 +1,4 @@
-package kr.youngminz.toby;
+package springbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/springbook/TobyApplicationTests.java
+++ src/test/java/springbook/TobyApplicationTests.java
@@ -1,4 +1,4 @@
-package kr.youngminz.toby;
+package springbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
