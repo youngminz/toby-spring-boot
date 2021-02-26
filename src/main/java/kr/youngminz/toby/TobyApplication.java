7c8b6cf32fd9c30ac48a0d8019a0be76ca5e6a3c
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-27 00:43:38 +0900

    Init project with Spring Initializr

---
+++ src/main/java/kr/youngminz/toby/TobyApplication.java
@@ -0,0 +1,13 @@
+package kr.youngminz.toby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/youngminz/toby/TobyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.youngminz.toby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
