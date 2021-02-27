ac5da56a7e780e7f42fbe99a9938bf4b90467297
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-28 03:54:30 +0900

    Update dao

--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -2,6 +2,7 @@ package springbook.user.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 import java.util.List;
@@ -18,11 +19,17 @@ public class UserDaoJdbc implements UserDao {
         user.setId(rs.getString("id"));
         user.setName(rs.getString("name"));
         user.setPassword(rs.getString("password"));
+        user.setLevel(Level.valueOf(rs.getInt("level")));
+        user.setLogin(rs.getInt("loign"));
+        user.setRecommend(rs.getInt("recommend"));
         return user;
     };
 
     public void add(User user) {
-        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
+        this.jdbcTemplate.update(
+                "insert into users(id, name, password, level, login, recommend) " +
+                        "values(?,?,?,?,?,?)",
+                user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend());
     }
 
     public User get(String id) {
