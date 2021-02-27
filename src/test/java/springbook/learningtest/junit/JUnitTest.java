6f4c2f15ea180250fce2a8d41e7a676b4e6d2dec
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-27 21:25:06 +0900

    Test application context is always same

--- src/test/java/springbook/learningtest/junit/JUnitTest.java
+++ src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -3,28 +3,47 @@ package springbook.learningtest.junit;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@SpringBootTest
+@ContextConfiguration(locations = "/junit.xml")
 public class JUnitTest {
+    @Autowired
+    ApplicationContext context;
+
     static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test1() {
         assertFalse(testObjects.contains(this));
         testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     public void test2() {
         assertFalse(testObjects.contains(this));
         testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
         assertFalse(testObjects.contains(this));
         testObjects.add(this);
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 }
