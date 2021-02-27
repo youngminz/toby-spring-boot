16b45b5963ad891708409dfb7776c5eaa63729c8
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-27 23:20:10 +0900

    Revert "Use XML Configuration in test"
    This reverts commit c4fc3d530d3bdb31c8833478c998bd100eec5a00.

--- src/test/java/springbook/user/dao/UserDaoTest.java
+++ src/test/java/springbook/user/dao/UserDaoTest.java
@@ -4,18 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 import springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.SQLException;
 
-@SpringBootTest
-@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
-    @Autowired
     private UserDao dao;
     private User user1;
     private User user2;
@@ -23,6 +19,11 @@ public class UserDaoTest {
 
     @BeforeEach
     public void setUp() {
+        dao = new UserDao();
+        DataSource dataSource = new SingleConnectionDataSource(
+                "jdbc:mysql://localhost/testdb", "spring", "book", true
+        );
+        dao.setDataSource(dataSource);
         this.user1 = new User("gyumee", "박성철", "springno1");
         this.user2 = new User("leegw700", "이길원", "springno2");
         this.user3 = new User("bumjin", "박범진", "springno3");
