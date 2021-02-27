98ec332eb8e8b367687df5c83b4194659045cdc3
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-27 22:48:55 +0900

    Apply strategy pattern to add method

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -14,17 +14,8 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
-        try (
-                Connection c = dataSource.getConnection();
-                PreparedStatement ps = c.prepareStatement(
-                        "insert into users(id, name, password) values (?,?,?)")) {
-
-            ps.setString(1, user.getId());
-            ps.setString(2, user.getName());
-            ps.setString(3, user.getPassword());
-
-            ps.executeUpdate();
-        }
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException {
