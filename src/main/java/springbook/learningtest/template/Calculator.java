bd17ae5802f20164955ed7f6f1cc3c12d82cd51b
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-28 01:38:03 +0900

    Implement calcMultiply

--- src/main/java/springbook/learningtest/template/Calculator.java
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -18,6 +18,22 @@ public class Calculator {
         return fileReadTemplate(filepath, sumCallback);
     }
 
+    public Integer calcMultiply(String filepath) throws IOException {
+        BufferedReaderCallback multiplyCallback = new BufferedReaderCallback() {
+            @Override
+            public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+                int multiply = 1;
+                String line;
+                while ((line = br.readLine()) != null) {
+                    multiply *= Integer.parseInt(line);
+                }
+                return multiply;
+            }
+        };
+
+        return fileReadTemplate(filepath, multiplyCallback);
+    }
+
     public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
             return callback.doSomethingWithReader(br);
