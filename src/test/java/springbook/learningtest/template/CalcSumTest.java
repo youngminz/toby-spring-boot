96f4e985b14610491641d89e92527dad3b9853df
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-28 01:33:03 +0900

    Extract object initialization to setUp

--- src/test/java/springbook/learningtest/template/CalcSumTest.java
+++ src/test/java/springbook/learningtest/template/CalcSumTest.java
@@ -2,15 +2,23 @@ package springbook.learningtest.template;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
 public class CalcSumTest {
+    Calculator calculator;
+    String numFilepath;
+
+    @BeforeEach
+    public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = getClass().getResource("/numbers.txt").getPath();
+    }
+
     @Test
     public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        int sum = calculator.calcSum(getClass().getResource("/numbers.txt").getPath());
-        assertEquals(sum, 10);
+        assertEquals(calculator.calcSum(this.numFilepath), 10);
     }
 }
