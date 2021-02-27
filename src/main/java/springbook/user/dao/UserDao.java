2be313e4bf3a06803a175a8a4c3a19c682ea3b09
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-28 03:26:56 +0900

    Create UserDao interface

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,51 +1,13 @@
 package springbook.user.dao;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
 import java.util.List;
 
-public class UserDao {
-    private JdbcTemplate jdbcTemplate;
-
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    private RowMapper<User> userMapper = (rs, rowNum) -> {
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setName(rs.getString("name"));
-        user.setPassword(rs.getString("password"));
-        return user;
-    };
-
-    public void add(User user) {
-        this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
-    }
-
-    public User get(String id) {
-        return this.jdbcTemplate.queryForObject(
-                "select * from users where id = ?",
-                userMapper,
-                id
-        );
-    }
-
-    public List<User> getAll() {
-        return this.jdbcTemplate.query(
-                "select * from users order by id",
-                userMapper
-        );
-    }
-
-    public void deleteAll() {
-        this.jdbcTemplate.update("delete from users");
-    }
-
-    public int getCount() {
-        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
-    }
+public interface UserDao {
+    void add(User user);
+    User get(String id);
+    List<User> getAll();
+    void deleteAll();
+    int getCount();
 }
-
