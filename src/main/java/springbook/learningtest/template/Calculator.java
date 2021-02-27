1714c7b7c52be576f269f95ccfb28c6d0fa04fef
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-28 01:28:47 +0900

    make fileReadTemplate

--- src/main/java/springbook/learningtest/template/Calculator.java
+++ src/main/java/springbook/learningtest/template/Calculator.java
@@ -6,13 +6,21 @@ import java.io.IOException;
 
 public class Calculator {
     public Integer calcSum(String filepath) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+        BufferedReaderCallback sumCallback = br -> {
             int sum = 0;
             String line;
             while ((line = br.readLine()) != null) {
                 sum += Integer.parseInt(line);
             }
             return sum;
+        };
+
+        return fileReadTemplate(filepath, sumCallback);
+    }
+
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
+            return callback.doSomethingWithReader(br);
         }
     }
 }
