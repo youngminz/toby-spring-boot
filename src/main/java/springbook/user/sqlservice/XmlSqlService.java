0bcb3f06000d0b369bff020c9bcff0eb93cb56ed
Youngmin Koo <youngminz.kr@gmail.com>
2021-03-01 05:14:42 +0900

    Move read xml logic to loadSql function and apply PostConstruct

--- src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -4,6 +4,7 @@ import com.epril.sqlmap.SqlType;
 import com.epril.sqlmap.Sqlmap;
 import springbook.user.dao.UserDao;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -13,13 +14,19 @@ import java.util.Map;
 
 public class XmlSqlService implements SqlService {
     private Map<String, String> sqlMap = new HashMap<>();
+    private String sqlmapFile;
 
-    public XmlSqlService() {
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+
+    @PostConstruct
+    public void loadSql() {
         String contextPath = Sqlmap.class.getPackage().getName();
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            InputStream is = UserDao.class.getResourceAsStream("/sqlmap.xml");
+            InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
             Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
             for (SqlType sql : sqlmap.getSql()) {
