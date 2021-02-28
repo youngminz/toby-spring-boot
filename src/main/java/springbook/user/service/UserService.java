0a71a2d9f13217b08e28ec66105c1860d2cdbd81
Youngmin Koo <youngminz.kr@gmail.com>
2021-03-01 03:16:33 +0900

    Use transaction annotation

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -1,12 +1,16 @@
 package springbook.user.service;
 
+import org.springframework.transaction.annotation.Transactional;
 import springbook.user.domain.User;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
     void add(User user);
+    @Transactional(readOnly = true)
     User get(String id);
+    @Transactional(readOnly = true)
     List<User> getAll();
     void deleteAll();
     void update(User user);
