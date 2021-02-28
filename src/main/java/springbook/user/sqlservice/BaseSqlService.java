f856857f510feb0e142b3b4624304fe953b30597
Youngmin Koo <youngminz.kr@gmail.com>
2021-03-01 05:44:28 +0900

    Separate self-referenced bean

--- src/main/java/springbook/user/sqlservice/BaseSqlService.java
+++ src/main/java/springbook/user/sqlservice/BaseSqlService.java
@@ -1,27 +1,10 @@
 package springbook.user.sqlservice;
 
-import com.epril.sqlmap.SqlType;
-import com.epril.sqlmap.Sqlmap;
-import springbook.user.dao.UserDao;
-
 import javax.annotation.PostConstruct;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
-public class BaseSqlService implements SqlService, SqlRegistry, SqlReader {
+public class BaseSqlService implements SqlService {
     protected SqlReader sqlReader;
     protected SqlRegistry sqlRegistry;
-
-    private Map<String, String> sqlMap = new HashMap<>();
-    private String sqlmapFile;
-
-    public void setSqlmapFile(String sqlmapFile) {
-        this.sqlmapFile = sqlmapFile;
-    }
 
     public void setSqlReader(SqlReader sqlReader) {
         this.sqlReader = sqlReader;
@@ -42,38 +25,6 @@ public class BaseSqlService implements SqlService, SqlRegistry, SqlReader {
             return this.sqlRegistry.findSql(key);
         } catch (SqlNotFoundException e) {
             throw new SqlRetrievalFailureException(e.getMessage());
-        }
-    }
-
-    @Override
-    public void registerSql(String key, String sql) {
-        sqlMap.put(key, sql);
-    }
-
-    @Override
-    public String findSql(String key) throws SqlNotFoundException {
-        String sql = sqlMap.get(key);
-        if (sql == null) {
-            throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다");
-        }
-
-        return sql;
-    }
-
-    @Override
-    public void read(SqlRegistry sqlRegistry) {
-        String contextPath = Sqlmap.class.getPackage().getName();
-        try {
-            JAXBContext context = JAXBContext.newInstance(contextPath);
-            Unmarshaller unmarshaller = context.createUnmarshaller();
-            InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
-            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
-
-            for (SqlType sql : sqlmap.getSql()) {
-                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
-            }
-        } catch (JAXBException e) {
-            throw new RuntimeException(e);
         }
     }
 }
---
+++ src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
@@ -0,0 +1,35 @@
+package springbook.user.sqlservice;
+
+import com.epril.sqlmap.SqlType;
+import com.epril.sqlmap.Sqlmap;
+import springbook.user.dao.UserDao;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+
+public class JaxbXmlSqlReader implements SqlReader {
+    private String sqlmapFile;
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+
+            for (SqlType sql : sqlmap.getSql()) {
+                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
