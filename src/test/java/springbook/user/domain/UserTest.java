2ad7576611368ac54a8bcaa1145efd08103e6d12
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-28 15:27:08 +0900

    Add level upgrade test

---
+++ src/test/java/springbook/user/domain/UserTest.java
@@ -0,0 +1,40 @@
+package springbook.user.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+    }
+
+    @Test
+    public void upgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() == null) {
+                continue;
+            }
+            user.setLevel(level);
+            user.upgradeLevel();
+            assertEquals(user.getLevel(), level.nextLevel());
+        }
+    }
+
+    @Test
+    public void cannotUpgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() != null) {
+                continue;
+            }
+            user.setLevel(level);
+            assertThrows(IllegalStateException.class, () -> user.upgradeLevel());
+        }
+    }
+}
