a572092aecdd85fb3a4f02a9ccd83e072a95937e
Youngmin Koo <youngminz.kr@gmail.com>
2021-03-01 05:07:03 +0900

    Insert all query into sqlmap.xml

---
+++ src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -0,0 +1,42 @@
+package springbook.user.sqlservice;
+
+import com.epril.sqlmap.SqlType;
+import com.epril.sqlmap.Sqlmap;
+import springbook.user.dao.UserDao;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class XmlSqlService implements SqlService {
+    private Map<String, String> sqlMap = new HashMap<>();
+
+    public XmlSqlService() {
+        String contextPath = Sqlmap.class.getPackage().getName();
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = UserDao.class.getResourceAsStream("/sqlmap.xml");
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+
+            for (SqlType sql : sqlmap.getSql()) {
+                sqlMap.put(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        String sql = sqlMap.get(key);
+        if (sql == null) {
+            throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다");
+        }
+
+        return sql;
+    }
+}
