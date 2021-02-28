57b1e8a137bccdd9950568c805c492c360cd0e67
Youngmin Koo <youngminz.kr@gmail.com>
2021-02-28 19:42:02 +0900

    Uppercase Proxy

--- src/test/java/springbook/learningtest/proxy/ProxyTest.java
+++ src/test/java/springbook/learningtest/proxy/ProxyTest.java
@@ -11,5 +11,10 @@ class ProxyTest {
         assertEquals(hello.sayHello("Toby"), "Hello Toby");
         assertEquals(hello.sayHi("Toby"), "Hi Toby");
         assertEquals(hello.sayThankYou("Toby"), "Thank You Toby");
+
+        Hello proxiedHello = new HelloUppercase(new HelloTarget());
+        assertEquals(proxiedHello.sayHello("Toby"), "HELLO TOBY");
+        assertEquals(proxiedHello.sayHi("Toby"), "HI TOBY");
+        assertEquals(proxiedHello.sayThankYou("Toby"), "THANK YOU TOBY");
     }
 }
