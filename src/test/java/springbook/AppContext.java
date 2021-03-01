a9817bf5e745fc6e33ccb2eca738cef5db585dd7
Youngmin Koo <youngminz.kr@gmail.com>
2021-03-02 03:18:08 +0900

    @Import to EnableSqlService; Why this does not work?

--- src/test/java/springbook/AppContext.java
+++ src/test/java/springbook/AppContext.java
@@ -23,7 +23,7 @@ import java.sql.Driver;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "springbook.user")
-@Import(SqlServiceContext.class)
+@EnableSqlService
 @PropertySource("/database.properties")
 public class AppContext implements SqlMapConfig {
     @Value("${db.driverClass}") Class<? extends Driver> driverClass;
