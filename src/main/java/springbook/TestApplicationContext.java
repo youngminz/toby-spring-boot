39f6db6d88a8507fb267cab4c57536bf174894e1
Youngmin Koo <youngminz.kr@gmail.com>
2021-03-02 01:01:28 +0900

    Move dataSource

--- src/main/java/springbook/TestApplicationContext.java
+++ src/main/java/springbook/TestApplicationContext.java
@@ -1,9 +1,25 @@
 package springbook;
 
+import com.mysql.cj.jdbc.Driver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ImportResource("/test-applicationContext.xml")
 public class TestApplicationContext {
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/testdb");
+        dataSource.setUsername("spring");
+        dataSource.setPassword("book");
+
+        return dataSource;
+    }
 }
